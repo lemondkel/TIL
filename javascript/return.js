d27395a48817e981ae91e9d6656bf6cacd4c091c
eunji <66556716+1o18z@users.noreply.github.com>
2022-11-19 13:27:22 +0900

    return example

---
+++ javascript/return.js
@@ -0,0 +1,29 @@
+function getTwo() {
+  return 40;
+}
+console.log(getTwo());
+
+
+
+function getTwo2(){
+  console.log(40);
+}
+getTwo2();
+
+
+
+console.log(40);
+
+
+
+function getTwo3(number) {
+  return number * 2;
+}
+console.log(getTwo3(20));
+
+
+let x = getTwo3(5);
+let y = getTwo3(2);
+console.log(x * y);
+
+// 40을 출력하는 4가지 방법
