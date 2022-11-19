33569e2131487a6f7357b06e4e80ed8fa91603b3
eunji <66556716+1o18z@users.noreply.github.com>
2022-11-19 15:39:26 +0900

    template example

---
+++ javascript/template.js
@@ -0,0 +1,17 @@
+let year = 2001;
+let month = 10;
+let day = 11;
+
+console.log('생년월일은' + year + '년' + month + '월' + day + '일 입니다.');
+console.log('생년월일은 ${year}년 ${month}월 ${day}일 입니다');
+// 같게 출력됨
+
+let myNumber = 3;
+
+function getTwice(x){
+  return x * 2;
+}
+
+console.log(`${myNumber}의 두 배는 ${getTwice(myNumber)} 입니다.`);
+
+// 템플릿 문자열 사용하면 더 쉽고 편하게 표현 가능
