b76579fbc2930010f20b14f0a87851df1a46d0ba
eunji <qkrdmswl1018@naver.com>
2022-11-25 21:25:34 +0900

    Update javascript

---
+++ javascript/WordGame.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('#input');
+
+function checker() {
+  const words = document.querySelectorAll('.word');
+  if (words.length === 0) {
+    alert('Success!👏');
+    if(confirm('retry?')) {
+      window.location.reload();
+    }
+  }
+}
+
+function removeWord(){
+  const word = document.querySelector(`[data-word="${input.value}"]`);
+  if (word) {
+    word.remove();
+    checker();
+  }
+  input.value='';
+}
+input.addEventListener('change',removeWord);
